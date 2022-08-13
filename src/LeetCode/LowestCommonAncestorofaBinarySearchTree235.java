70839ff24b4d9029b1c27a7583b6d3a53d30b0c3
ivasuy <vasu7yadav@gmail.com>
2022-08-13 13:26:26 +0530

    Leetcode Daily

--- src/LeetCode/LowestCommonAncestorofaBinarySearchTree235.java
+++ src/LeetCode/LowestCommonAncestorofaBinarySearchTree235.java
@@ -3,25 +3,25 @@ package LeetCode;
 import javax.swing.tree.TreeNode;
 
 public class LowestCommonAncestorofaBinarySearchTree235 {
-//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//
-//        if ( root == null || root == p || root == q ){
-//            return root;
-//        }
-//
-//        TreeNode left = lowestCommonAncestor(root.left, p, q);
-//        TreeNode right = lowestCommonAncestor(root.right, p, q);
-//
-//        if ( left == null ){
-//            return right;
-//        }
-//        if ( right == null ){
-//            return left;
-//        }
-//        else {
-////             both left and right are not null we got our answere
-//            return root;
-//        }
-//
-//    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if ( root == null || root == p || root == q ){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if ( left == null ){
+            return right;
+        }
+        if ( right == null ){
+            return left;
+        }
+        else {
+//             both left and right are not null we got our answere
+            return root;
+        }
+
+    }
 }
