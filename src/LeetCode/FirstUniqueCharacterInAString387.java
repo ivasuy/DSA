00c0f1691d18d77272140f854207dc6626acafad
Vasu Yadav <107919164+ivasuy@users.noreply.github.com>
2022-08-16 15:57:57 +0530

    Update FirstUniqueCharacterInAString387.java

--- src/LeetCode/FirstUniqueCharacterInAString387.java
+++ src/LeetCode/FirstUniqueCharacterInAString387.java
@@ -4,12 +4,13 @@ public class FirstUniqueCharacterInAString387 {
 
     static public int firstUniqChar(String s) {
 
-        for ( char c : s.toCharArray() ){
+        for ( char c : s.toCharArray() ){ 
 
             int index = s.indexOf(c);
             int lastIndex = s.lastIndexOf(c);
-
-            if ( index == lastIndex ){
+            
+// because if my first occurrence is my last occurrence I found my answere
+            if ( index == lastIndex ){ 
                 return index;
             }
         }
@@ -17,3 +18,5 @@ public class FirstUniqueCharacterInAString387 {
         return -1;
     }
 }
+
+// complexity o(n^2)
