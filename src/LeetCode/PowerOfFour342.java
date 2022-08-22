4ae0b436f318e9ad1798ffab79bb35ca90491a56
Vasu Yadav <107919164+ivasuy@users.noreply.github.com>
2022-08-22 18:31:12 +0530

    Leetcode Daily

---
+++ src/LeetCode/PowerOfFour342.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+public class PowerOfFour342 {
+    public boolean isPowerOfFour(int n) {
+
+        if ( n == 1 ){
+
+            return true;
+
+        }if ( n == 0 || n % 4 != 0 ){
+
+            return false;
+
+        }
+
+        return isPowerOfFour(n/4);
+
+    }
+}
